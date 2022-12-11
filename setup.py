ed86bfbdfe740d156d0c0d56f20c2cd3a691554c
JanekDeveloper <103539203+JanekDeveloper@users.noreply.github.com>
2022-12-11 21:08:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["disnake==2.7.0"]
 
 setup(
     name="discocrash.py",
-    version="0.0.3",
+    version="0.0.4",
     author="JanekDeveloper",
     author_email="ivanbogynsky@gmail.com",
     description="Module for creating discord crash bots",
